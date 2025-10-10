cb6f6d375a56a62ad2f56f75cdb0fb632f5859e0
Ashley <an943@drexel.edu>
2025-10-10 13:04:20 -0400

    create alpha build
    styled build to try and match original
    coded javascript toggle for annotation module
    cropped microinteraction gif

--- Alpha/Build/postload.js
+++ Alpha/Build/postload.js
@@ -1,2 +1,11 @@
 //obj
-const annotated = document.getElementById('firstlyric');
+const firsthighlight = document.getElementById('firstlyric');
+const annotation = document.getElementById('FirstPopUp');
+const clicked = document.getElementsByClassName('clicked')
+
+//toggle annotation
+firsthighlight.addEventListener('click', function(){ 
+    annotation.classList.toggle('hideMe');
+    firsthighlight.classList.toggle('clicked');
+});
+
